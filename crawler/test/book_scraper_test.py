230fb7b1f497ec92763881b13039a55e4fd16fdf
Jeff Xiong <gigix1980@gmail.com>
2012-03-03 17:17:12 +1100

    first test

--- crawler/test/book_scraper_test.py
+++ crawler/test/book_scraper_test.py
@@ -1,2 +1,5 @@
-def test_():
-	pass
+from ..model.book_scraper import BookScraper
+
+def test_scrape_reader_ids_of_give_book():
+	scraper = BookScraper(1140457)
+	assert len(scraper.readers()) >= 1064
