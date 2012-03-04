70c002c5b7dc2427a221e003b3394229c7cef008
Jeff Xiong <gigix1980@gmail.com>
2012-03-04 16:06:08 +1100

    Crawler is running now

---
+++ crawler/model/crawler.py
@@ -0,0 +1,19 @@
+from Queue import Queue
+
+from scrapers import BookScraper, ReaderScraper
+
+class Crawler:
+	def __init__(self, book_id, basedir="data", limit=1000):
+		self.limit = limit
+		self.basedir = basedir
+		self.scrapers_queue = Queue()
+		self.scrapers_queue.put(BookScraper(book_id))
+		
+	def run(self):
+		while(self.limit > 0 and not self.scrapers_queue.empty()):
+			self.limit -= 1
+			scraper = self.scrapers_queue.get()
+			scraper.persistent(self.basedir)
+			for new_scraper in scraper.spawn():
+				self.scrapers_queue.put(new_scraper)
+			print "Scraped item " + scraper.id + ", " + str(self.limit) + " items to go."
---
+++ crawler/run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from model.crawler import Crawler
+
+Crawler('1766670').run()
